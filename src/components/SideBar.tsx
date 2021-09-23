4f567adb7cc2b57abf68bf8d5adf490bcc4781a5
John Everton <johneverton44@gmail.com>
2021-09-22 22:02:12 -0300

    create sideBar and content component

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,3 +1,34 @@
-export function SideBar() {
-  // Complete aqui
+import { useEffect, useState } from "react";
+import { Button } from '../components/Button';
+import { api } from '../services/api';
+interface GenreResponseProps {
+  id: number;
+  name: 'action' | 'comedy' | 'documentary' | 'drama' | 'horror' | 'family';
+  title: string;
+}
+
+interface GenrePrps {
+  genres: GenreResponseProps[],
+  selectedGenreId: number,
+  handleClickButton: Function
+}
+
+
+
+
+
+export function SideBar({ genres, selectedGenreId, handleClickButton  }: GenrePrps) {
+  return (
+    <div className="buttons-container">
+          {genres.map(genre => (
+            <Button
+              key={String(genre.id)}
+              title={genre.title}
+              iconName={genre.name}
+              onClick={() => handleClickButton(genre.id)}
+              selected={selectedGenreId === genre.id}
+            />
+          ))}
+    </div>
+  )
 }
